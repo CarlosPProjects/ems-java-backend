6484779eb4295d1fcc86739a0110758fd3a4286c
CarlosPProjects <cg9822@gmail.com>
2024-11-03 23:50:18 +0100

    Created User entity and relation between User and Role

--- src/main/java/net/javaguides/ems/entity/User.java
+++ src/main/java/net/javaguides/ems/entity/User.java
@@ -1,11 +1,31 @@
 package net.javaguides.ems.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username",nullable = false, unique=true)
     private String username;
 
+    @Column(name="password", nullable = false)
     private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
 
 }
