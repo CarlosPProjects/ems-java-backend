8ff3f9cec6706d65a6e3d98550050a55617be991
CarlosPProjects <cg9822@gmail.com>
2024-11-04 18:44:02 +0100

    Updated SecurityConfig to return a http status 401 unauthorized if user is not authenticated.

--- src/main/java/net/javaguides/ems/config/SecurityConfig.java
+++ src/main/java/net/javaguides/ems/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package net.javaguides.ems.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +25,10 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .build();
     }
 }
