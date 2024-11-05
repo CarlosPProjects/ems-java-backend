bd115a0094b58e294fb7377470bd7452cb1df3f2
CarlosPProjects <cg9822@gmail.com>
2024-11-05 17:51:17 +0100

    sss

--- src/main/java/net/javaguides/ems/service/impl/AuthServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/AuthServiceImpl.java
@@ -9,6 +9,10 @@ import net.javaguides.ems.entity.User;
 import net.javaguides.ems.jwt.JwtServiceImpl;
 import net.javaguides.ems.mapper.UserMapper;
 import net.javaguides.ems.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +22,17 @@ public class AuthServiceImpl implements net.javaguides.ems.service.AuthService {
 
     private final UserRepository userRepository;
     private final JwtServiceImpl jwtServiceImpl;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public AuthResponse loginUser(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtServiceImpl.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     @Override
@@ -30,11 +41,16 @@ public class AuthServiceImpl implements net.javaguides.ems.service.AuthService {
 
         User user = UserMapper.mapToUser(userDto);
 
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         user.setRole(Role.HR);
 
         userRepository.save(user);
 
+        String token = jwtServiceImpl.getToken(user);
+
         return AuthResponse.builder()
-                .token(jwtServiceImpl.getToken(user)).build();
+                .token(token)
+                .build();
     }
 }
